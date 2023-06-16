66cbb24980d81eba6a0a3e4cc4cdce71954a73ca
Jane Saldana <janesaldana@Janes-Mac-mini.local>
2023-06-16 17:51:00 -0500

    updated tab2and3

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,38 @@
+import { useState } from 'react'
+import {puppyList} from './data.js'
 
 
 import './App.css'
 
 function App() {
- 
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupID, setFeatPupId] = useState(null);
+
+//console.log (puppies)
+const featuredPup = puppies.find((pup)=> pup.id === featPupID)
+console.log (featuredPup)
 
   return (
-    <>
-    
-      <h1>Vite + JaneSFIRSTReact</h1>
-      
-    </>
+    <div className='App'>
+        {
+          puppies.map((puppy)  => {
+            return <p onClick={()=> {setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
+          })
+          
+        }
+
+        { featPupID && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>  
+            
+        )}
+        
+    </div>
   )
 }
 
